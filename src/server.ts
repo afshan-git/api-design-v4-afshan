e07de32ca266a328dd4e2f5661cddb8576342bad
V A Afshan <123004009@sastra.ac.in>
2023-02-06 12:52:28 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -46,7 +46,7 @@ app.use((err,req,res,next) => {
         res.status(401).json({message: 'unauthorized'})
     }
     else if(err.type === 'input'){
-        res.status(400).json({message: 'invalid input 333'})
+        res.status(400).json({message: `invalid input 333, ${err}`})
     }else{
         res.status(500).json({message: 'oops thats on us'})
     }
